6de378afd218dbeb9d8c61dbdb0a81981b3cb141
archy_yu <archy_yu@sina.com>
2018-11-23 23:17:25 +0800

    Add files via upload

---
+++ src/main/java/com/archy/secorder/SecorderApplication.java
@@ -0,0 +1,23 @@
+package com.archy.secorder;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@MapperScan("com.mingyang.secorder.dao.mapper")
+@SpringBootApplication
+public class SecorderApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecorderApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(SecorderApplication.class);
+    }
+
+}
